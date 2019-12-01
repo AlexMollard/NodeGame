dd61d7014606aa2329415af17fa95499a9422bb6
Alex Mollard <alex.mollard@mail.com>
2019-12-01 22:56:41 +1100

    First Commit
    Added the ability to place a node that will auto connect to closest node and are able to move node by dragging it, this will also update the connections so that they stay connected from any distance.

---
+++ NodeGame/Assets/Scripts/Node.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public List<Node> ConnectedNodes;
+    public LineRenderer Line;
+    public bool CanGive = false;
+    public bool isPressed = false;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        ConnectedNodes = new List<Node>();
+    }
+    List<Node> GetConnectedNodes()
+    {
+        return null;
+    }
+    public void AddConnection(Node newNode)
+    {
+        ConnectedNodes.Add(newNode);
+    }
+    public void createConnection()
+    {
+        List<Vector3> poses = new List<Vector3>();
+        poses.Add(transform.position);
+        for (int i = 0; i < ConnectedNodes.Count; i++)
+        {
+            poses.Add(ConnectedNodes[i].transform.position);
+        }
+        Vector3[] newPostitions = poses.ToArray();
+        Line.SetPositions(newPostitions);
+    }
+    private void Update()
+    {
+        if (isPressed)
+        {
+            Vector2 MousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 objPosition = Camera.main.ScreenToWorldPoint(MousePosition);
+            transform.position = objPosition;
+            createConnection();
+            for (int i = 0; i < ConnectedNodes.Count; i++)
+            {
+                ConnectedNodes[i].createConnection();
+            }
+        }
+    }
+    private void OnMouseDown()
+    {
+        isPressed = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isPressed = false;
+    }
+}
---
+++ NodeGame/Assets/Scripts/NodeManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeManager : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+
+    public GameObject NodePrefab;
+    public List<Node> Nodes;
+
+    void Start()
+    {
+        Nodes = new List<Node>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 screenPoint = Input.mousePosition;
+            screenPoint.z += 10.0f;
+            screenPoint = Camera.main.ScreenToWorldPoint(screenPoint);
+
+            int total = 0;
+            for (int i = 0; i < Nodes.Count; i++)
+            {
+                if (Vector3.Distance(Nodes[i].transform.position, screenPoint) < 0.5f)
+                {
+                    break;
+                }
+                total++;
+            }
+
+            if (total == Nodes.Count)
+            {
+                CreateNode(screenPoint);
+            }
+        }
+    }
+
+
+
+    void CreateNode(Vector3 screenPoint)
+    {
+        GameObject newNodeObject = Instantiate(NodePrefab, screenPoint, Quaternion.identity);
+        newNodeObject.transform.parent = this.transform;
+        newNodeObject.name = "Node: " + screenPoint;
+        Node NewNode = newNodeObject.GetComponent<Node>();
+
+        if (Nodes.Count > 0)
+        {
+            float ClosestDistance = float.MaxValue;
+            Node ClosestNode = null;
+            for (int i = 0; i < Nodes.Count; i++)
+            {
+                if (Vector2.Distance(Nodes[i].transform.position, NewNode.transform.position) < ClosestDistance)
+                {
+                    ClosestDistance = Vector2.Distance(Nodes[i].transform.position, NewNode.transform.position);
+                    ClosestNode = Nodes[i];
+                }
+            }
+
+            NewNode.AddConnection(ClosestNode);
+            ClosestNode.AddConnection(NewNode);
+
+            NewNode.createConnection();
+        }
+        Nodes.Add(NewNode);
+    }
+}
